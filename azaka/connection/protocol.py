a828dfa3ecbd3e1ce347ff10e9ece9ab08786ff2
unknown <importz750@gmail.com>
2021-11-26 22:14:41 +0530

    Adds a mixin on transporter for queue and event handling

--- azaka/connection/protocol.py
+++ azaka/connection/protocol.py
@@ -14,23 +14,23 @@ logger = logging.getLogger(__name__)
 
 class Protocol(asyncio.Protocol):
 
-    __slots__ = ("_subscriber", "command", "on_connect", "on_disconnect")
+    __slots__ = ("_listener", "command", "on_connect", "on_disconnect")
 
     def __init__(
         self, command: bytes, on_connect: asyncio.Event, on_disconnect: asyncio.Event
     ) -> None:
-        self._subscriber: t.Optional[t.Callable[[str], t.NoReturn]] = None
+        self._listener: t.Optional[t.Callable[[str], t.NoReturn]] = None
         self.command = command
         self.on_connect = on_connect
         self.on_disconnect = on_disconnect
 
     @property
-    def subscriber(self):
-        return self._subscriber
+    def listener(self):
+        return self._listener
 
-    @subscriber.setter
-    def subscriber(self, value: t.Callable[[str], t.NoReturn]) -> None:
-        self._subscriber = value
+    @listener.setter
+    def listener(self, value: t.Callable[[str], t.NoReturn]) -> None:
+        self._listener = value
 
     def connection_made(self, transport: transports.Transport) -> None:  # type: ignore
         logger.info(
@@ -48,7 +48,7 @@ class Protocol(asyncio.Protocol):
             self.on_connect.set()
 
         elif (response.type == "results") or (response.type == "dbstats"):
-            self.subscriber(response.data)
+            self.listener(response.data)
 
         else:
             self.on_disconnect.set()
