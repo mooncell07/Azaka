9e61b30cd1519175221cfd45ee9316808e224675
Emiya97 <importz750@gmail.com>
2022-01-06 01:08:36 +0530

    Adds context docs.

--- azaka/context.py
+++ azaka/context.py
@@ -13,6 +13,19 @@ __all__ = ("Context",)
 
 
 class Context:
+    """
+    A context holding all the necessary info and objects to connect to the VNDB API.
+    This same object is given to the 0th arg of every coroutine function decorated
+    with [Client.register](../API-Reference/client.md#azaka.client.Client.register).
+
+    Attributes:
+        ADDR (str): The address of the VNDB API.
+        PORT (int): The port to which client should connect. Azaka only supports `19535` as it provides TLS support.
+        PROTOCOL_VERSION (int): The version of the protocol this client is using. Currently, this is `1`.
+        CLIENT_NAME (str): The name of the client. Defaults to `Azaka`.
+        CLIENT_VERSION (str): The version of the client.
+
+    """
 
     ADDR = "api.vndb.org"
     PORT = 19535
@@ -31,6 +44,15 @@ class Context:
         loop: t.Optional[asyncio.AbstractEventLoop] = None,
         ssl_context: t.Optional[ssl.SSLContext] = None
     ) -> None:
+        """
+        Context Constructor. This is where all the necessary info and object are stored.
+
+        Args:
+            username: Username to use for logging in.
+            password: Password to use for logging in.
+            loop: The [asyncio.AbstractEventLoop][] subclass to use.
+            ssl_context: The [ssl.SSLContext][] to use. If not provided, a default context will be used.
+        """
         self.username = username
         self.password = password
 
