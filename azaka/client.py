0894b521998ef4c64d8ac4ffe168759a3483f1c6
Nova <mooncell07@proton.me>
2022-01-06 00:37:22 +0530

    Adds client docs.

--- azaka/client.py
+++ azaka/client.py
@@ -56,10 +56,26 @@ class Client(Presets):
         *,
         username: t.Optional[str] = None,
         password: t.Optional[str] = None,
-        loop: t.Optional[asyncio.BaseEventLoop] = None,
+        loop: t.Optional[asyncio.AbstractEventLoop] = None,
         ssl_context: t.Optional[ssl.SSLContext] = None,
         cache_size: int = 50,
     ) -> None:
+        """
+        Client Constructor. This is the main entry point for the library.
+
+        Args:
+            username: Username to use for logging in.
+            password: Password to use for logging in.
+            loop: The [asyncio.AbstractEventLoop][] subclass to use.
+            ssl_context: The [ssl.SSLContext][] to use. If not provided, a default context will be used.
+            cache_size: Size of the internal cache of data. Defaults to 50.
+
+        Info:
+            If you have uvloop installed, then the lib will by default use uvloop's event loop.
+
+        Note:
+            A password or session token should be provided if passing an username.
+        """
         self.ctx: Context = Context(
             username=username, password=password, loop=loop, ssl_context=ssl_context
         )
@@ -70,6 +86,10 @@ class Client(Presets):
 
     @property
     def is_closing(self) -> bool:
+        """
+        Returns:
+            True if the client's [asyncio.Transport][] is closing/closed.
+        """
         return (
             self._connector.transport is None
             or self._connector.transport.is_closing()  # noqa
@@ -79,6 +99,23 @@ class Client(Presets):
         self,
         coro: t.Callable[..., t.Any],
     ) -> None:
+        """
+        Register a coroutine to be called when client is ready to issue commands.
+
+        Args:
+            coro: The coroutine to call.
+
+        Info:
+            The coroutine must be a function that takes a single argument of type [Context][].
+
+        Example:
+            ```py
+            @register
+            async def my_coro(ctx):
+                ...
+            ```
+        """
+
         if inspect.iscoroutinefunction(coro):
             self.ctx.loop.create_task(
                 self._connector.handle_user_exceptions(coro(self.ctx))
@@ -87,9 +124,37 @@ class Client(Presets):
             raise TypeError("Callback must be a coroutine.") from None
 
     def on_error(self, func: t.Callable[..., t.Any]) -> None:
+        """
+        Register a function to be called when an error occurs.
+
+        Args:
+            func: The function to call.
+
+        Info:
+            The function must take a single argument of type [AzakaException][].
+
+        Example:
+            ```py
+            @on_error
+            def my_error_handler(error):
+                ...
+            ```
+        """
         self._connector.error_listener(func)
 
     def start(self, *, token: t.Optional[t.Union[bool, str]] = None) -> None:
+        """
+        Start the client and attempt to login.
+
+        Args:
+            token: The token to use. If not provided, password will be used if username is provided.
+
+        Raises:
+            AzakaException: Raises when a username was passed but not a password or session token.
+
+        Note:
+            For fetching a token, the argument should be set to `True` and username and password must be provided.
+        """
         data = {
             "protocol": self.ctx.PROTOCOL_VERSION,
             "client": self.ctx.CLIENT_NAME,
@@ -122,6 +187,12 @@ class Client(Presets):
             pass
 
     async def logout(self) -> None:
+        """
+        logout and close the client's connection. Only works when client was connected using a session token.
+
+        Raises:
+            AzakaException: Raises when there is no session token available.
+        """
         command = Command("logout")
         token = self._cache.get("token")
 
@@ -149,6 +220,15 @@ class Client(Presets):
         return result
 
     async def dbstats(self, update: bool = False) -> DBStats:
+        """
+        Get the VNDB database statistics.
+
+        Args:
+            update: If set to `True`, the client makes an api call to get data else it returns cached data.
+
+        Returns:
+            [DBStats][]
+        """
         command = Command("dbstats")
 
         if command not in self._cache or update is True:
@@ -163,9 +243,17 @@ class Client(Presets):
 
     async def get(
         self, interface: Interface, paginate=False, **kwargs: bool
-    ) -> t.Union[
-        t.Iterable[BaseObject], Paginator, t.Tuple[t.Iterable[BaseObject], bool, int]
-    ]:
+    ) -> t.Union[t.Iterable[BaseObject], Paginator]:
+        """
+        Issue a `get` command to the API.
+
+        Args:
+            interface: The [Interface][] to use.
+            paginate: If set to `True`, the client will return a [Paginator][] instance.
+
+        Returns:
+            [list][] of [BaseObject][] if paginate is set to `False` else [Paginator][] if paginate is set to `True`.
+        """
         if paginate:
             return Paginator(self, interface)
 
@@ -178,11 +266,34 @@ class Client(Presets):
         obj = [_model_selector(interface._type)(data) for data in result["items"]]
 
         if kwargs.get("metadata"):
-            return (obj, result["more"], result["num"])
+            return (obj, result["more"], result["num"])  # type: ignore
 
         return obj
 
-    async def set_ulist(self, id: int, **kwargs: t.Mapping[str, t.Any]) -> ResponseType:
+    async def set_ulist(self, id: int, **kwargs: str) -> ResponseType:
+        """
+        Issue a `set` command to the API. This method is used to set userlist data.
+
+        Args:
+            id: The item id.
+            **kwargs: The data to set.
+
+        Info:
+            The following keys are supported for kwargs:
+
+                - notes
+
+                - started
+
+                - finished
+
+                - vote
+
+                - labels
+
+        Returns:
+            [ResponseType][]
+        """
         command = Command(f"set ulist {id}", **kwargs)
 
         future = self.ctx.loop.create_future()
@@ -194,9 +305,22 @@ class Client(Presets):
     async def get_extra_info(
         self, *args: str, default: t.Optional[bool] = None
     ) -> t.Optional[t.List[t.Any]]:
+        """
+        Get extra information about the client's internal [asyncio.Transport][].
+
+        Args:
+            args: The extra information to get.
+            default: The default value to return if the information is not available.
+
+        Returns:
+            [list][] of extra information.
+        """
         return await self._connector.get_extra_info(args, default=default)
 
     def stop(self) -> None:
+        """
+        Stops the event loop and closes the connection.
+        """
         self.ctx.loop.stop()
 
     def __repr__(self) -> str:
