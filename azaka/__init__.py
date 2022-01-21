d0b3700c6b77530351270e3cee9ec982f8cc5396
Nova <mooncell07@proton.me>
2022-01-21 17:44:04 +0530

    Docs and ver. bump

--- azaka/__init__.py
+++ azaka/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0a6"
+__version__ = "0.1.0"
 
 from .client import *
 from .tools import *
--- azaka/connection/protocol.py
+++ azaka/connection/protocol.py
@@ -27,15 +27,25 @@ if t.TYPE_CHECKING:
 __all__ = ("Protocol",)
 logger = logging.getLogger(__name__)
 
-HIGH_WATERMARK = 64
-LOW_WATERMARK = 16
-
 
 class Protocol(asyncio.Protocol):
+    """
+    This is the [asyncio.Protocol][] that azaka implements
+    to communicate with the server.
+
+    Warning:
+        This class is not meant to be used directly in any way.
+    """
 
     __slots__ = ("_command", "_errors", "connector")
 
     def __init__(self, connector: Connector) -> None:
+        """
+        Protocol constructor.
+
+        Args:
+            connector: The connector that is used to communicate with the server.
+        """
         self.connector = connector
         self._errors = {
             "filter": CommandFilterError,
@@ -53,6 +63,14 @@ class Protocol(asyncio.Protocol):
 
     @property
     def command(self) -> t.Optional[bytes]:
+        """
+        A property to get the first command to be sent to the server.
+        This is called only once in the lifetime by the protocol and
+        carries the necessary details to establish the connection.
+
+        Returns:
+            The first command to be sent to the server.
+        """
         return self._command
 
     @command.setter
@@ -60,25 +78,62 @@ class Protocol(asyncio.Protocol):
         self._command = value
 
     def connection_made(self, transport: transports.Transport) -> None:  # type: ignore
-        transport.set_write_buffer_limits(high=HIGH_WATERMARK, low=LOW_WATERMARK)
+        """
+        The [asyncio.BaseProtocol.connection_made][] override which
+        writes the first command to the transport.
+
+        Args:
+            transport: The transport that is used to communicate with the server.
+        """
         transport.write(self.command)
         logger.info(f"DISPATCHED TRANSPORTER WITH {repr(self.command)}")
 
     def pause_writing(self) -> None:
+        """
+        The [asyncio.BaseProtocol.pause_writing][] override which is called when the buffer reaches
+        the high watermark. This method calls [asyncio.Event.clear][] to apply backpressure
+        to the server.
+        """
+        logger.warning("TRANSPORT IS PAUSING DUE TO BUFFER OVERFLOW.")
         self.connector.push_back.clear()
 
     def resume_writing(self) -> None:
+        """
+        The [asyncio.BaseProtocol.resume_writing][] override which is called when the buffer
+        is empty enough to resume writing. This method calls [asyncio.Event.set][] to
+        release backpressure from the server.
+        """
         self.connector.push_back.set()
+        logger.info("TRANSPORT RESUMED.")
 
     def data_received(self, data: bytes) -> None:
+        """
+        The [asyncio.Protocol.data_received][] override which is called when data is received.
+
+        Args:
+            data: The data received from the server.
+        """
         logger.info("PAYLOAD RECEIVED.")
         self._direct(data)
 
-    def connection_lost(self, exc: t.Optional[Exception]) -> None:
+    def connection_lost(self, _: t.Optional[Exception]) -> None:
+        """
+        The [asyncio.BaseProtocol.connection_lost][] override which is called
+        when the connection is lost or explicitly closed.
+
+        Args:
+            _: The exception that caused the connection to be lost.
+        """
         logger.info("CONNECTION CLOSED.")
         self.connector.on_disconnect.set()
 
     def _direct(self, data: bytes) -> None:
+        """
+        A method to manage the data received from the server.
+
+        Args:
+            data: The data received from the server.
+        """
         response = Response(data)
 
         if response.type in {ResponseType.OK, ResponseType.SESSION}:
