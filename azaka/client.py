5bc24a6dad6d4106b6500d67de3d9d3d39ca92a0
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2023-07-03 00:27:18 +0530

    feat: add dynamic object generation

--- azaka/client.py
+++ azaka/client.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 import aiohttp
 from query import Query
 
@@ -17,9 +19,29 @@ class Client:
     async def __aexit__(self, exc, exc_val, tb):
         await self.cs.close()
 
-    async def execute(self, query: Query):
-        response = await self.cs.post(query.route, data=query.body)
-        return response
+    async def execute(self, query: Query, json: bool = False):
+        resp = await self.cs.post(query.url, data=query.body)
+
+        if json:
+            obj = await self._get_data(resp)
+        else:
+            obj = await self._make_object(query, resp)
+
+        return obj["results"]
+
+    async def _get_data(self, resp: aiohttp.ClientResponse):
+        if 400 > resp.status >= 200:
+            return await resp.json()
+        else:
+            raise NotImplementedError(await resp.text())
+
+    async def _make_object(self, query: Query, resp: aiohttp.ClientResponse):
+        structs = []
+        jsons = (await self._get_data(resp))["results"]
+        for json in jsons:
+            struct = namedtuple(query._route.upper(), json)
+            structs.append(struct(*json.values()))
+        return structs
 
     async def create_cs(self):
         self.cs = aiohttp.ClientSession(headers={"Content-Type": "application/json"})
