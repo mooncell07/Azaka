db13b75793d24fd39c34a2dda45e6e59fd1235fa
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2023-07-03 15:33:44 +0530

    feat: add all GET methods and their respective models add support for user authentication

--- azaka/__init__.py
+++ azaka/__init__.py
@@ -3,3 +3,4 @@ __version__ = "0.4.0"
 from .client import *
 from .query import *
 from .exceptions import *
+from .models import *
--- azaka/client.py
+++ azaka/client.py
@@ -3,18 +3,20 @@ from collections import namedtuple
 from types import TracebackType
 
 import aiohttp
+import query
 from exceptions import EXMAP, AzakaException
-from query import Query
+from models import AuthInfo, Stats, User
 from typing_extensions import Self
+from yarl import URL
 
 __all__ = ("Client",)
 
 
 class Client:
-    __slots__ = ("key", "cs")
+    __slots__ = ("base_header", "cs")
 
     def __init__(self, key: t.Optional[str] = None) -> None:
-        self.key = key
+        self.base_header = {"Authorization": f"token {key}"} if key else None
 
     async def __aenter__(self) -> Self:
         await self.create_cs()
@@ -28,10 +30,47 @@ class Client:
     ) -> None:
         await self.cs.close()
 
+    async def get_schema(self) -> dict[str, str]:
+        resp = await self.cs.get(query.SCHEMA_URL)
+        data = await self._get_data(resp)
+        return data
+
+    async def get_stats(self) -> Stats:
+        resp = await self.cs.get(query.STATS_URL)
+        data = await self._get_data(resp)
+        return Stats(**data)
+
+    async def get_auth_info(self) -> AuthInfo:
+        if not self.base_header:
+            raise TypeError("Missing required argument 'key'")
+        resp = await self.cs.get(query.AUTHINFO_URL, headers=self.base_header)
+        data = await self._get_data(resp)
+        return AuthInfo(**data)
+
+    async def get_user(self, *users: str, fields: t.Sequence[str] = ()) -> t.Sequence[User]:
+        url = URL(query.USER_URL).update_query({"q": users, "fields": fields})
+        resp = await self.cs.get(url)
+        data = await self._get_data(resp)
+        user_list = []
+
+        for user in data:
+            u = (
+                User(search_term=user, **data[user], FOUND=True)
+                if data[user]
+                else User(search_term=user, FOUND=False)
+            )
+            user_list.append(u)
+        return user_list
+
     async def execute(
-        self, query: Query, json: bool = False
+        self, query: query.Query, json: bool = False
     ) -> t.Sequence[dict[str, t.Any]] | t.Sequence[t.NamedTuple]:
-        resp = await self.cs.post(query.url, data=query.body)
+        if self.base_header:
+            resp = await self.cs.post(
+                query.url, data=query.body, headers=self.base_header
+            )
+        else:
+            resp = await self.cs.post(query.url, data=query.body)
 
         if json:
             return (await self._get_data(resp))["results"]
@@ -51,7 +90,7 @@ class Client:
                 raise AzakaException(msg, status)
 
     async def _make_object(
-        self, query: Query, resp: aiohttp.ClientResponse
+        self, query: query.Query, resp: aiohttp.ClientResponse
     ) -> t.Sequence[t.NamedTuple]:
         structs = []
         jsons = (await self._get_data(resp))["results"]
