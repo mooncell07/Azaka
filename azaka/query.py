836282b6c6e66e5b93d9c0e3e55e78484f2ada7e
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2023-07-04 18:25:24 +0530

    feat: add test for Client.execute() fix: typehints and return types

--- azaka/query.py
+++ azaka/query.py
@@ -1,10 +1,12 @@
 import enum
-import typing as t
+import functools
 import json
-from utils import clean_string
+import typing as t
+from dataclasses import dataclass, field
 
 from typing_extensions import Self
-from dataclasses import dataclass, field
+
+from azaka.utils import FT, clean_string
 
 __all__ = ("select", "AND", "OR", "Node")
 BASE = "https://api.vndb.org/kana"
@@ -17,25 +19,38 @@ AUTHINFO_URL = f"{BASE}/authinfo"
 USER_URL = f"{BASE}/user"
 
 
-def AND(*args: tuple) -> list:
+def AND(*args: FT[str]) -> FT[str]:
     return ["and", *args]
 
 
-def OR(*args: tuple) -> list:
+def OR(*args: FT[str]) -> FT[str]:
     return ["or", *args]
+
+
+class Body(t.TypedDict):
+    filters: FT[str]
+    fields: str
+    sort: str
+    reverse: bool
+    results: int
+    page: int
+    user: t.Optional[str]
+    count: bool
+    compact_filters: bool
+    normalized_filters: bool
 
 
 class Query:
     __slots__ = ("_route", "_body")
 
-    def __init__(self, route: str ="", body={}) -> None:
+    def __init__(self, route: str = "", body: t.Optional[Body] = None) -> None:
         self._route = route
-        self._body = body or self._defaults()
+        self._body: Body = body or self._defaults()
 
-    def _defaults(self):
+    def _defaults(self) -> Body:
         return {
             "filters": [],
-            "fields": "id",
+            "fields": "id, ",
             "sort": "id",
             "reverse": False,
             "results": 10,
@@ -50,7 +65,7 @@ class Query:
         self._route = clean_string(route)
         return self
 
-    def where(self, filters: list) -> Self:
+    def where(self, filters: FT[str]) -> Self:
         self._body["filters"] = filters
         return self
 
@@ -60,11 +75,11 @@ class Query:
 
     def set_flags(
         self,
-        reverse=False,
-        count=False,
-        compact_filters=False,
-        normalized_filters=False,
-    ):
+        reverse: bool = False,
+        count: bool = False,
+        compact_filters: bool = False,
+        normalized_filters: bool = False,
+    ) -> None:
         self._body["reverse"] = reverse
         self._body["count"] = count
         self._body["compact_filters"] = compact_filters
@@ -85,36 +100,39 @@ class Node:
     def __init__(self, name: str) -> None:
         self.name = clean_string(name)
 
-    def _fmt(self, op: str, val: str | list) -> list:
+    def guard(self, value: object) -> t.TypeGuard[str | FT[str]]:
+        return isinstance(value, (str, list))
+
+    def _fmt(self, op: str, val: str | FT[str]) -> FT[str]:
         return [self.name, op, val]
 
     def __eq__(self, val: object):
-        if isinstance(val, (str, list)):
+        if self.guard(val):
             return self._fmt("=", val)
 
     def __ne__(self, val: object):
-        if isinstance(val, (str, list)):
+        if self.guard(val):
             return self._fmt("!=", val)
 
     def __gt__(self, val: object):
-        if isinstance(val, (str, list)):
+        if self.guard(val):
             return self._fmt(">", val)
 
     def __ge__(self, val: object):
-        if isinstance(val, (str, list)):
+        if self.guard(val):
             return self._fmt(">=", val)
 
     def __lt__(self, val: object):
-        if isinstance(val, (str, list)):
+        if self.guard(val):
             return self._fmt("<", val)
 
     def __le__(self, val: object):
-        if isinstance(val, (str, list)):
+        if self.guard(val):
             return self._fmt("<=", val)
 
 
 def select(*fields: str) -> Query:
     query = Query()
     if fields:
-        query._body["fields"] = ", ".join(fields)
+        query._body["fields"] += ", ".join(fields)
     return query
