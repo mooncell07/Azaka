d99c1a42f499f1f92a61f89c851a9e0027984569
mooncell07 <80042274+mooncell07@users.noreply.github.com>
2023-07-06 13:37:11 +0530

    docs: Add docs and change branding

--- azaka/client.py
+++ azaka/client.py
@@ -18,11 +18,26 @@ class Client:
     __slots__ = ("cs", "token")
 
     def __init__(self, token: t.Optional[str] = None) -> None:
+        """
+        Client Constructor.
+
+        Args:
+            token: VNDB API access token.
+
+        Attributes:
+            cs (t.Optional[aiohttp.ClientSession]): An aiohttp.ClientSession object.
+        """
         self.token = token
         self.cs: t.Optional[aiohttp.ClientSession] = None
 
     @property
     def base_header(self) -> t.Optional[t.Mapping[str, str]]:
+        """
+        Return a dict. containing Auth. token.
+
+        Note:
+            It is [None][] when a token is not supplied.
+        """
         return {"Authorization": f"token {self.token}"} if self.token else None
 
     async def __aenter__(self) -> t.Self:
@@ -38,16 +53,39 @@ class Client:
         await self.close_cs()
 
     async def get_schema(self) -> t.Mapping[str, str]:
+        """
+        Return a Mapping containing metadata about API objects and enums.
+
+        Returns:
+            data: Mapping of API structure.
+        """
         resp = await self._request(query.SCHEMA_URL)
         data = await self._get_data(resp)
         return t.cast(t.Mapping[str, str], data)
 
     async def get_stats(self) -> Stats:
+        """
+        Return VNDB database stats.
+
+        Returns:
+            stats: A [dataclasses.dataclass][] representation of the [json][] response.
+        """
         resp = await self._request(query.STATS_URL)
         data = await self._get_data(resp)
         return Stats(**data)
 
     async def get_auth_info(self) -> AuthInfo:
+        """
+        Validate the API token and return information about it.
+
+        Returns:
+            authinfo: A [dataclasses.dataclass][] representation of the [json][] response.
+        
+        Note:
+            - A [TypeError][] is raised if the token is not passed.
+
+            - InvalidAuthTokenError will be raised by the API if token is found invalid.
+        """
         if not self.base_header:
             raise TypeError("Missing required argument 'token'")
         resp = await self._request(query.AUTHINFO_URL, headers=self.base_header)
@@ -57,6 +95,34 @@ class Client:
     async def get_user(
         self, *users: str, fields: t.Sequence[str] = ()
     ) -> t.Sequence[User]:
+        """ 
+        Find a user by id or username and return information about them.
+    
+        Args:
+            users: Variable number of username/ids to search for.
+            fields: A Sequence type holding all fields to select.
+
+        Accepted fields for the `fields` parameter are:
+
+        - id
+
+        - username
+
+        - lengthvotes
+
+        - lengthvotes_sum
+
+        Note:
+            The `id` and `username` fields are always selected and 
+            should not be explicitly specified.
+
+        Returns:
+            user_list: A [list][] of User objects.
+
+        Tip:
+            Since API supports multiple user lookup using just one query, you can pass multiple
+            users like so: `await client.get_user("u1", "u2", .....)`
+        """
         url = URL(query.USER_URL).update_query({"q": users, "fields": fields})
         resp = await self._request(url)
         data = await self._get_data(resp)
@@ -72,6 +138,38 @@ class Client:
         return user_list
 
     async def execute(self, query: query.Query) -> Response:
+        """
+        Return a Response object containing the results of the query and associated metadata.
+
+        This method is used for searching and querying any database entry. It supports all the 
+        query routes exposed by the API publicly.
+
+
+        Note:
+            This method dynamically generates the Response.results attribute. 
+            Only the fields that you specify in the select() function during the 
+            query will be present in the list of results.
+
+        Note:
+            Fields that access data inside another data structure, such as image.url, 
+            will have the left part become the attribute of the result and the data
+            structure associated with the right part will become it's value.
+            For example, the Response may look like this:
+
+            `Response(results=[VN(id="v2", image={"url": ...})], ...)`
+        
+            The wrapper does not handle nested data. 
+
+        Tip:
+            If you are new, I recommend reading ... to get a clear 
+            understanding of how to build queries.
+
+        Args:
+            query: A Query object.
+
+        Returns:
+            response: A Response object.
+        """
         if not query._route:
             raise TypeError("'route' cannot be empty")
 
@@ -115,5 +213,12 @@ class Client:
             )
 
     async def close_cs(self) -> None:
+        """
+        Close the internal [aiohttp.ClientSession].
+
+        Danger:
+            You must call this method after completing the request if you are not using
+            Context Manager.
+        """
         if self.cs:
             await self.cs.close()
